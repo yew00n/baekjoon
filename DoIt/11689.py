9d6002ec54cffc6bf18e48327017b8836e2b5caa
yew00n <yewoon.dev@gmail.com>
2023-05-09 22:48:12 +0900

    230509

---
+++ DoIt/11689.py
@@ -0,0 +1,14 @@
+import math
+n = int(input())
+result = n
+
+for p in range(2, int(math.sqrt(n))+1):
+    if n % p == 0:
+        result -= result / p
+        while n % p == 0:
+            n /= p
+
+if n > 1:
+    result -= result / n
+
+print(int(result))
